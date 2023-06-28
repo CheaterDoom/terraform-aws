5c591653f39c4a0b9d087e06aae676231e6e3f7f
Erlant Farias <eng.erlant@gmail.com>
2023-06-27 22:54:27 -0300

    lab-03

---
+++ lab-03-terraform/variables.tf
@@ -0,0 +1,22 @@
+variable "environment" {
+
+  type        = string
+  description = ""
+  default     = "dev"
+
+
+}
+variable "aws_profile" {
+
+  type        = string
+  description = ""
+  default     = "default"
+
+}
+variable "aws_region" {
+
+  type        = string
+  description = ""
+  default     = "sa-east-1"
+
+}
